3216a9c27d07b3d2e4d1d1179a194d5561f896f9
Muthukumar0769 <muthukumar@hubino.com>
2026-04-20 18:56:43 +0530

    deployes db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,22 +1,13 @@
-import { Sequelize } from "sequelize";
-import dotenv from "dotenv";
+import {Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const dbName = process.env.MYSQLDATABASE || process.env.DB_NAME;
-const dbUser = process.env.MYSQLUSER || process.env.DB_USER;
-const dbPassword = process.env.MYSQLPASSWORD || process.env.DB_PASS;
-const dbHost = process.env.MYSQLHOST || process.env.DB_HOST;
-const dbPort = Number(process.env.MYSQLPORT || process.env.DB_PORT || 3306);
-
 export const sequelize = new Sequelize(
-  dbName as string,
-  dbUser as string,
-  dbPassword as string,
-  {
-    host: dbHost as string,
-    port: dbPort,
-    dialect: "mysql",
-    logging: false,
-  }
+ process.env.DB_URL as string,{
+    dialect:"mysql",
+    logging:false
+ }
 );
+
+
